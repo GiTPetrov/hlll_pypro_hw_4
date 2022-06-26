8fd4010e366b213a2cbc469fb44034eb9d0b86da
“Maksym” <max.orel@gmail.com>
2022-06-26 14:07:24 +0200

    django, tutorial 01

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3&=#0-uw&dy)fyv*m@fw_r#)_a#u!h9&edz-m!sm5tga!b8(&k'
+SECRET_KEY = os.environ['HW4_SK']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
